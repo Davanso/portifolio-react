c3af5634bb361d895a168323e4893114f5ebbfbe
Gui <gui.social.ti@gmail.com>
2024-07-24 23:21:30 -0300

    Add Home page (as Hero.tsx), and the features and external packs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import Hero from "../../components/Hero";
+import { FloatingNav } from "../../components/ui/FloatingNavBar";
+import { FaHome } from "react-icons/fa";
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 scroll-px-52">
       <div className="max-w-7xl w-full">
-        <h1>Hello Portifolio</h1>
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome/>}
+        ]} />
+        <Hero />
       </div>
     </main>
   );
