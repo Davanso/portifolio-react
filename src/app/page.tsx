cf806072b7a61c0c2e049cf117815579f5d521d0
Gui <gui.social.ti@gmail.com>
2024-07-25 23:20:13 -0300

    80% work done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
-import Image from "next/image";
+"use client";
+
+import { navItems } from "../../data";
+
+
+import Grid from "../../components/Grid";
+import Footer from "../../components/Footer";
+import Approach from "../../components/Approach";
+import Experience from "../../components/Experience";
+import RecentProjects from "../../components/RecentProjects";
 import Hero from "../../components/Hero";
 import { FloatingNav } from "../../components/ui/FloatingNavBar";
-import { FaHome } from "react-icons/fa";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 scroll-px-52">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome/>}
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
